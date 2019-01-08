e1b4ba8dd99fb8571a42eb424f54bdd1f2fdd739
Danny Tobin <daniel.joseph.tobin@gmail.com>
2019-01-08 10:14:14 -0500

    add the bonus cube, squre and square_n_times functions

--- calculator.py
+++ calculator.py
@@ -13,3 +13,17 @@ def divide(a, b):
 print("I'm going to use the calculator to divide 64 and 8")
 x = divide(64, 8)
 print(x)
+
+def square(a):
+	return a ** 2
+
+def cube(a):
+	return a ** 3
+
+def square_n_times(number, n):
+	new_num = number
+	i = 0
+	while i < n:
+		new_num = number ** 2
+		i += 1
+	return new_num
